b1fb5bca3ecc1f89bf399bd1ae2b323064232136
hauhuynh1208 <hauhuynhvnn@gmail.com>
2024-03-30 00:22:00 +0700

    Map chart

--- src/MapComponent.tsx
+++ src/MapComponent.tsx
@@ -3,12 +3,11 @@ import * as am5 from "@amcharts/amcharts5";
 import * as am5map from "@amcharts/amcharts5/map";
 import am5geodata_worldLow from "@amcharts/amcharts5-geodata/worldLow";
 import { Button, ButtonGroup, CardBody } from "react-bootstrap";
-import { groupDataByProduct } from "./utils/generators";
+import { groupDataByCountry } from "./utils/generators";
 import { data } from "./data";
-import { TableData } from "./interface";
+import { MapData, TableData } from "./interface";
 
 const MapComponent = () => {
-  const chartTypes: string[] = ["bar", "pie", "line"];
   const dimensions: string[] = [
     "transactions",
     "value",
@@ -16,31 +15,69 @@ const MapComponent = () => {
     "containers",
     "weight",
   ];
-  const [chartType, setChartType] = React.useState<string>("bar");
   const [dimension, setDimension] = React.useState<string>("transactions");
-  const [_data, setData] = React.useState<Array<TableData>>([]);
+  const [_data, setData] = React.useState<Array<MapData>>([]);
 
+  function transformData(rawData: MapData[]) {
+    return rawData.map((d: MapData) => ({
+      ...d,
+      id: d.countryCode,
+      polygonSettings: {
+        fill: am5.color("#512da8"),
+      },
+    }));
+  }
   React.useEffect(() => {
-    setData(groupDataByProduct(data));
-  }, []);
-
+    const result = transformData(groupDataByCountry(data)).map(
+      (_dataGroupedByCountry) => {
+        return {
+          ..._dataGroupedByCountry,
+          data: _dataGroupedByCountry.data.filter((d) => d[dimension] !== 0),
+        };
+      }
+    );
+    setData(result);
+  }, [dimension]);
   React.useLayoutEffect(() => {
     let root = am5.Root.new("chartMap");
     let chart = root.container.children.push(
       am5map.MapChart.new(root, {
-        projection: am5map.geoNaturalEarth1(),
+        panX: "translateX",
+        panY: "translateY",
+        projection: am5map.geoMercator(),
       })
     );
     chart.set("zoomControl", am5map.ZoomControl.new(root, {}));
+    chart.chartContainer.get("background")?.events.on("click", function () {
+      chart.goHome();
+    });
     let polygonSeries = chart.series.push(
       am5map.MapPolygonSeries.new(root, {
         geoJSON: am5geodata_worldLow,
+        exclude: ["AQ"],
       })
     );
+    polygonSeries.mapPolygons.template.setAll({
+      tooltipText: "{country}:\n {id}",
+      toggleKey: "active",
+      interactive: true,
+      fill: am5.color("#eeeeee"),
+      stroke: am5.color("#757575"),
+      templateField: "polygonSettings",
+    });
+
+    polygonSeries.mapPolygons.template.states.create("hover", {
+      fill: root.interfaceColors.get("primaryButtonHover"),
+    });
+    polygonSeries.mapPolygons.template.states.create("active", {
+      fill: root.interfaceColors.get("primaryButtonHover"),
+    });
+    polygonSeries.data.setAll(_data);
+    chart.appear(1000, 100);
     return () => {
       root.dispose();
     };
-  }, [data]);
+  }, [_data, dimension]);
   return (
     <CardBody className="d-flex flex-column">
       <div className="d-flex" style={{ flex: 1 }} id="chartMap"></div>
--- src/utils/generators.ts
+++ src/utils/generators.ts
@@ -1,5 +1,5 @@
 import { metrics } from "../constants";
-import { Data, TableData } from "../interface";
+import { Data, MapData, TableData } from "../interface";
 
 export function groupDataByProduct(rawData: Data[]): TableData[] {
   const products = Array.from(
@@ -21,6 +21,43 @@ export function groupDataByProduct(rawData: Data[]): TableData[] {
       _result = { ..._result, ...obj };
     }
     result.push(_result as TableData);
+  }
+  return result;
+}
+
+export function groupDataByCountry(rawData: Data[]): MapData[] {
+  function getCountryCode(countryName: string): string {
+    return rawData.filter((_rawData) => _rawData.country === countryName)[0]
+      .countryCode;
+  }
+
+  const countries: string[] = Array.from(
+    new Set(rawData.map((_rawData) => _rawData.country))
+  );
+  const products: string[] = Array.from(
+    new Set(rawData.map((_rawData) => _rawData.product))
+  );
+  let result: MapData[] = [];
+  for (let country of countries) {
+    const dataAfterFilteredByCountry = rawData.filter(
+      (_rawData) => _rawData.country === country
+    );
+    const countryCode = getCountryCode(country);
+    let _result: MapData = { country, countryCode, data: [] };
+    for (let product of products) {
+      let productAndMetrics: any = { product };
+      for (let metric of metrics) {
+        let obj = {
+          [metric]: dataAfterFilteredByCountry
+            .filter((_data) => _data.product === product)
+            .reduce((acc: any, obj) => acc + obj[metric], 0),
+        };
+        productAndMetrics = { ...productAndMetrics, ...obj };
+      }
+      _result.data.push(productAndMetrics);
+    }
+
+    result.push(_result);
   }
   return result;
 }
